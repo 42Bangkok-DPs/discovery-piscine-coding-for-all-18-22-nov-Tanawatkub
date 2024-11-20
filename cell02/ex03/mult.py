64f2f17d83ba58abdc35fe4f8205fd3a4f4f58c4
Tanawatkub <67030091@kmitl.ac.th>
2024-11-20 16:13:49 +0000

    gg

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+number = int(input("Enter the first number:"))
+number_second = int(input("Enter the second number"))
+result = number * number_second
+
+print(f"{number} x {number_second} = {result}")
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
