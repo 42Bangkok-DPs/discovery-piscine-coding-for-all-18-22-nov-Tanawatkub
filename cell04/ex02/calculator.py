6d465125010479b83b9caf399c9ea7574039720b
Tanawatkub <67030091@kmitl.ac.th>
2024-11-21 15:16:20 +0000

    gg

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+frist_number = float(input("Give me the first number: "))
+second_number = float(input("Give me the second number: "))
+print("Thank you!")
+
+print(f"{frist_number} + {second_number} = {frist_number + second_number}")
+print(f"{frist_number} - {second_number} = {frist_number - second_number}")
+print(f"{frist_number} / {second_number} = {frist_number / second_number}")
+print(f"{frist_number} * {second_number} = {frist_number * second_number}")
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import math
+
+number = float(input("Give me a number: "))
+round_up = math.ceil(number)
+
+print(round_up)
